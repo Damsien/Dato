7bc8244ccb1845798303e9523380911a275875b6
Tom Froment <tomfromtls@gmail.com>
2022-01-05 11:10:55 +0100

    Add packages

---
+++ src/pile/p_liste.adb
@@ -0,0 +1,115 @@
+with Ada.Text_IO; use Ada.Text_IO; 
+
+PACKAGE BODY p_liste IS
+
+   -- FONCTIONS
+    FUNCTION creerListeVide RETURN T_LISTE IS
+        l : T_LISTE;
+    BEGIN
+        l := Null;
+        RETURN l;
+    END creerListeVide;
+
+    FUNCTION estVide(l : IN T_LISTE) RETURN Boolean IS
+    BEGIN
+        RETURN l = Null;
+    END estVide;
+
+    PROCEDURE insererEnTete(l : IN OUT T_LISTE ; e : IN T_ELEMENT) IS
+        copie : T_LISTE;
+    BEGIN
+        copie := new T_CELLULE'(e,l);
+        l := copie;
+    END insererEnTete;
+
+    PROCEDURE afficherListe(l : IN T_LISTE) IS
+    BEGIN
+        IF l /= Null THEN
+            Put(Image(l.All.Element));
+            Put_Line("");
+            afficherListe(l.All.Suivant);
+        ELSE
+            Null;
+        END IF;
+    END afficherListe;
+
+    FUNCTION rechercher(l : IN T_LISTE ; e : IN T_ELEMENT) RETURN T_LISTE IS
+        liste : T_LISTE := l;
+    BEGIN
+        WHILE liste /= Null AND THEN liste.All.Element /= e LOOP
+            liste := liste.All.Suivant;
+        END LOOP;
+        
+        RETURN liste;
+    END rechercher;
+
+    PROCEDURE insererApres(l : IN T_LISTE ; e : IN T_ELEMENT ; data : IN T_ELEMENT) IS
+        ListeVide, DataNonPresent : Exception;
+        copie : T_LISTE := l;
+    BEGIN
+        IF Not estVide(l) THEN
+            WHILE copie /= Null AND THEN copie.All.Element /= data LOOP
+                copie := copie.All.Suivant;
+            END LOOP;
+            
+            IF estVide(copie) THEN
+                raise DataNonPresent;
+            ELSE
+                copie.All.Suivant := new T_CELLULE'(e,copie.All.Suivant);
+            END IF;                
+        ELSE
+            raise ListeVide;
+        END IF;
+    EXCEPTION
+        WHEN ListeVide => Put_Line("La liste vide");
+        WHEN DataNonPresent => Put_Line("L'element recherche n'est pas present");
+    END insererApres;
+
+    PROCEDURE insererAvant(l : IN OUT T_LISTE ; e : IN T_ELEMENT ; data : IN T_ELEMENT) IS
+        ListeVide, DataNonPresent : Exception;
+        copie : T_LISTE := l;
+    BEGIN
+        IF Not estVide(l) THEN
+            IF l.All.Element = data THEN
+                insererEnTete(l,e);
+            ELSE
+                WHILE copie.All.Suivant /= Null AND THEN copie.All.Suivant.All.Element /= data LOOP
+                    copie := copie.All.Suivant;
+                END LOOP;
+                
+                IF estVide(copie.All.Suivant) THEN
+                    raise DataNonPresent;
+                ELSE
+                    copie.All.Suivant := new T_CELLULE'(e,copie.All.Suivant);
+                END IF;            
+            END IF;    
+        ELSE
+            raise ListeVide;
+        END IF;
+    EXCEPTION
+        WHEN ListeVide => Put_Line("La liste vide");
+        WHEN DataNonPresent => Put_Line("L'element recherche n'est pas present");
+    END insererAvant;
+
+    PROCEDURE enlever(l : IN OUT T_LISTE ; e : IN T_ELEMENT) IS
+        copie : T_LISTE := l;
+    BEGIN
+        IF Not estVide(l) THEN
+            IF l.All.Element = e THEN
+                l := l.All.Suivant;
+            ELSE
+                WHILE copie.All.Suivant /= Null AND THEN copie.All.Suivant.All.Element /= e LOOP
+                    copie := copie.All.Suivant;
+                END LOOP;
+            END IF;
+            
+            IF copie.All.Suivant.Element = e THEN
+                copie.All.Suivant := copie.All.Suivant.All.Suivant;
+            ELSE
+                Null;
+            END IF;
+        ELSE
+            Null;
+        END IF;
+    END;
+END p_liste;
---
+++ src/pile/p_liste.ads
@@ -0,0 +1,34 @@
+GENERIC
+   TYPE T_ELEMENT IS PRIVATE;
+   WITH FUNCTION Image(element : IN T_ELEMENT) RETURN String;
+PACKAGE p_liste IS
+   -- TYPES
+   TYPE T_CELLULE;
+   TYPE T_LISTE IS ACCESS T_CELLULE;
+   TYPE T_CELLULE IS RECORD
+        Element : T_ELEMENT;
+        Suivant : T_LISTE;
+   END RECORD;
+
+   -- FONCTIONS
+    FUNCTION creerListeVide RETURN T_LISTE
+        WITH Post => estVide(creerListeVide'Result);
+
+    FUNCTION estVide(l : IN T_LISTE) RETURN Boolean;
+
+    PROCEDURE insererEnTete(l : IN OUT T_LISTE ; e : IN T_ELEMENT)
+        WITH Post => rechercher(l,e) /= Null;
+
+    PROCEDURE afficherListe(l : IN T_LISTE);
+
+    FUNCTION rechercher(l : IN T_LISTE ; e : IN T_ELEMENT) RETURN T_LISTE;
+
+    PROCEDURE insererApres(l : IN T_LISTE ; e : IN T_ELEMENT ; data : IN T_ELEMENT)
+        WITH Post => rechercher(l,e) /= Null;
+
+    PROCEDURE insererAvant(l : IN OUT T_LISTE ; e : IN T_ELEMENT ; data : IN T_ELEMENT)
+        WITH Post => rechercher(l,e) /= Null;
+
+    PROCEDURE enlever(l : IN OUT T_LISTE ; e : IN T_ELEMENT)
+        WITH Post => rechercher(l,e) = Null;   
+END p_liste;
